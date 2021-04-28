1cbbcabd881e990f19b59ba1b325ab07d7905619
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2021-04-28 09:29:46 -0700

    Rename package to mpfdmc (MPF Direct Media Controller)

--- mpfdmc/server_pb2_grpc.py
+++ mpfdmc/server_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from . import server_pb2 as server__pb2
+from mpfdmc import server_pb2 as server__pb2
 
 
 class MediaControllerStub(object):
