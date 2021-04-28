ffd99a3b2162caea47bcea11d78dd441c22fcd2e
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2021-04-28 16:49:50 -0700

    Move server py files into server folder pending next update with config_players

--- mpfdmc/generate.sh
+++ mpfdmc/generate.sh
@@ -1,1 +1,1 @@
-python3 -m grpc_tools.protoc -I../proto --python_out=. --grpc_python_out=. ../proto/server.proto
+python3 -m grpc_tools.protoc -I../proto --python_out=./server --grpc_python_out=./server ../proto/server.proto
